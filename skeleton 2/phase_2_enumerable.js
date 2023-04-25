9bca72b1bd061eb3204c54df75f21198798733f5
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-04-25 16:43:27 -0400

    hardest part

--- skeleton 2/phase_2_enumerable.js
+++ skeleton 2/phase_2_enumerable.js
@@ -1,14 +1,48 @@
-const callback = function(ele) {
+const multiplyer = function(ele) {
+    // console.log(ele * 2)
     return ele * 2
-    };
+};
+
+const toSquare = function(ele) {
+    return ele * ele
+};
 
 
-Array.prototype.myMap = function(banana) {
+
+
+
+Array.prototype.myMap = function(block) {
     let result = [];
-    for(let i = 0; i < this.length; i++) {
-        result.push(banana(this[i]));
-    };
+
+    // for(let i = 0; i < this.length; i++) {
+    //     result.push(block(this[i]));
+    // };
+
+    // return this.myEach(block);
+    this.myEach(ele => result.push(block(ele)));
+    // this.myEach(function(ele) {
+    //     result.push(block(ele))
+    // });
+    // block = multiplyer 
+    // myEach cb = (ele => result.push(block(ele)))
+
     return result;
+};
+
+Array.prototype.myEach = function(someBlock) {
+    // let result = [];
+
+    for(let i = 0; i < this.length; i++) {
+        // result.push(block(this[i]));
+
+            
+        someBlock(this[i]);
     };
 
+    // return result;
+};
 
+
+// let testCase = [1, 2, 3, 4]
+
+// console.log(testCase.myEach(multiplyer))
